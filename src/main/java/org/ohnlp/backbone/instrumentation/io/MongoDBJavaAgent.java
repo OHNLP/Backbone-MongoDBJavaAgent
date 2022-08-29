cdd0e8c76420d75298046acecdc7efdc7940d736
Andrew Wen <Wen.Andrew@mayo.edu>
2022-08-29 13:40:20 -0500

    Initial Commit

---
+++ src/main/java/org/ohnlp/backbone/instrumentation/io/MongoDBJavaAgent.java
@@ -0,0 +1,16 @@
+package org.ohnlp.backbone.instrumentation.io;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.lang.instrument.Instrumentation;
+
+public class MongoDBJavaAgent {
+    public static void premain(String arg, Instrumentation inst) {
+        new AgentBuilder.Default()
+                // Redefine AggregateIterableImpl to run sorts on disk by default
+                .type(ElementMatchers.named("com.mongodb.client.internal.AggregateIterableImpl"))
+                .transform((builder, type, ignored, ignored2) -> builder.field(ElementMatchers.named("allowDiskUse")).value(true))
+                .installOn(inst);
+    }
+}
